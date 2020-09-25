d4d25874effdcf2fdf1ae9d4581ad070ca2d969b
wrzosdev <wrzosdev@gmail.com>
2020-09-25 12:48:37 +0200

    Formularz decode

--- php/contact-form.php
+++ php/contact-form.php
@@ -1,7 +1,7 @@
 <?php
 $to_email = 'wrzosdev@gmail.com';
 $subject = 'wrzos.dev contact form';
-$message = serialize(utf8_decode($_POST));
+$message = utf8_decode(serialize($_POST));
 $headers = 'From: wrzosinf@wn28.webd.pl';
 $result = mail($to_email,$subject,$message,$headers);
 if(!$result) {
