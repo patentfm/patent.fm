82c17fdf77407b77d33316f2d8643f3c9a784a64
wrzosdev <wrzosdev@gmail.com>
2020-10-01 01:25:45 +0200

    Wrzos.dev v0.0050 Poprawki php i brakujący layout error

--- _site/php/contact.php
+++ _site/php/contact.php
@@ -59,7 +59,7 @@ $mail->Body = $msg;
 if (!$mail->send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
-    echo "?email=".$_POST["email"]."&name=".$_POST["name"];
+    echo "success";
 }
 
 //echo "success";
