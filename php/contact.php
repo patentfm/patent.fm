d1c6b2868c2286fc6ac037195be4053e923b8e49
wrzosdev <wrzosdev@gmail.com>
2020-10-01 16:48:05 +0200

    Wrzos.dev v100 CORS

--- php/contact.php
+++ php/contact.php
@@ -1,7 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 error_reporting(0);
 /*
  * ------------------------------------
