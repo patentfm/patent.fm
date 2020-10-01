7aca3cb29cc6fd9b13f8b5bde05e3d0e93bfac36
wrzosdev <wrzosdev@gmail.com>
2020-10-01 17:34:00 +0200

    Wrzos.dev v125  Segmentacja form

--- php/contact.php
+++ php/contact.php
@@ -1,6 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 error_reporting(0);
 /*
  * ------------------------------------
