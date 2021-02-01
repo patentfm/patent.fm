c4b77f6e3fd98f8a869e0d43c76ea8646b123760
Arkadiusz Wrzos <wrzosdev@gmail.com>
2021-02-02 00:27:25 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -74,7 +74,7 @@ $(function() {
               url: "https://formspree.io/f/wrzosdev@gmail.com",
               method: "POST",
               dataType: "json",
-              data: $(form).serialize(),
+              data: $(this).serializeArray().reduce(function(m,o){  m[o.name] = o.value; return m;}, {}),
                 success: function(d) {
                     if (d.startsWith("Mailer")) {
                         $jscontactresult.fadeIn('slow').html('<div class="mt-3 help-block text-danger">' + error_msg + '</div>').delay(10000).fadeOut('slow');
